988935c573bb309fb2836df5107974e4348151f9
DandaraF <dandara-chr@hotmail.com>
2022-01-31 13:38:50 -0300

    add imgs project

---
+++ src/assets/img/index.js
@@ -0,0 +1,8 @@
+import add from "./add.svg";
+import background from "./background.svg";
+import email from "./email.svg";
+import logo from "./logo.svg";
+import more from "./more.svg";
+import search from "./search.svg";
+
+export { add, background, email, logo, more, search };
